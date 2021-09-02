7c7b29871b6fb10dc100e3a2ac702e7a278e8982
quadrant26 <quadrant26@163.com>
2021-09-02 17:00:44 +0800

    feat: jwt 验证方法

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserService } from './logical/user/user.service';
 import { UserModule } from './logical/user/user.module';
-import { AuthService } from './logical/auth/auth.service';
 import { AuthModule } from './logical/auth/auth.module';
+import { UserController } from './logical/user/user.controller';
 
 @Module({
   imports: [UserModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService, UserService, AuthService],
+  controllers: [AppController, UserController],
+  providers: [AppService],
 })
 export class AppModule {}
