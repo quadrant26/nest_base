ae5c0f48cd899602dc3d4974c89011f01d26e1f1
quadrant26 <quadrant26@163.com>
2021-09-02 15:57:51 +0800

    chroe: 创建 auth 模块

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserService } from './logical/user/user.service';
 import { UserModule } from './logical/user/user.module';
+import { AuthService } from './logical/auth/auth.service';
+import { AuthModule } from './logical/auth/auth.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService, UserService],
+  providers: [AppService, UserService, AuthService],
 })
 export class AppModule {}
