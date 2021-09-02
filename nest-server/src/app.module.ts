ffbd472407dcbb4b3112ba4ccaceec9ca6be8d66
quadrant26 <quadrant26@163.com>
2021-09-02 14:01:38 +0800

    chroe: 在 src 下新建 logical 文件夹，创建 user 模块

--- nest-server/src/app.module.ts
+++ nest-server/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserService } from './logical/user/user.service';
+import { UserModule } from './logical/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
---
+++ nest-server/src/logical/user/user.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import * as Sequelize from 'sequelize';
+import sequelize from '../../database/sequelize';
+
+@Injectable()
+export class UserService {
+  async findOne(username: string): Promise<any | undefined> {
+    const sql = `
+      SELECT 
+        user_id id real_name realName, role
+      From
+        admin_user
+      WHERE
+        account_name = '${username}'
+    `;
+
+    try {
+      const res = await sequelize.query(sql, {
+        type: Sequelize.QueryTypes.SELECT,
+        raw: true,
+        logging: true,
+      });
+
+      const user = res[0];
+      if (user) {
+        return {
+          code: 200,
+          data: {
+            user,
+          },
+          msg: 'success',
+        };
+      } else {
+        return {
+          code: 600,
+          msg: '查无此人',
+        };
+      }
+    } catch (error) {
+      return {
+        code: 503,
+        msg: `Service error: ${error}`,
+      };
+    }
+  }
+}
