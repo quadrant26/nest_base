8f17433da747f16523e472a261a04f0a009efa82
quadrant26 <quadrant26@163.com>
2021-09-03 11:16:25 +0800

    feat: 使用 pipe 校验

---
+++ nest-server/src/pipe/validation.pipe.ts
@@ -0,0 +1,33 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
+
+@Injectable()
+export class ValidationPipe implements PipeTransform {
+  async transform(value: any, { metatype }: ArgumentMetadata) {
+    console.log(`value:`, value, 'metatype: ', metatype);
+    if (!metatype || !this.toValidate(metatype)) {
+      // 如果没有传入验证规则，则不验证，直接返回数据
+      return value;
+    }
+    // 将对象转换为 Class 来验证
+    const object = plainToClass(metatype, value);
+    const errors = await validate(object);
+
+    if (errors.length > 0) {
+      const msg = Object.values(errors[0].constraints)[0];
+      throw new BadRequestException(`Validation failed: ${msg}`);
+    }
+    return value;
+  }
+
+  private toValidate(metatype: any): boolean {
+    const types: any[] = [String, Boolean, Number, Array, Object];
+    return !types.includes(metatype);
+  }
+}
